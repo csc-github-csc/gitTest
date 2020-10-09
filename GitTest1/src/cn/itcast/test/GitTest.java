8322361c198e67d5081266434e8db736301b880b
Adam <Adam@qq.com>
2020-10-09 22:37:52 +0800

    第三次提交，修改GitTest.java里面的代码

--- GitTest1/src/cn/itcast/test/GitTest.java
+++ GitTest1/src/cn/itcast/test/GitTest.java
@@ -3,5 +3,6 @@ package cn.itcast.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("======Hello Git!=====");
+        System.out.println("==================");
     }
 }
